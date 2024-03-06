01256cce4ede14e3f45dc001e3c3191c36f849f4
Mohammed-Atef-20231143 <161865675+Mohammed-Atef-20231143@users.noreply.github.com>
2024-03-06 18:04:10 +0200

    Number Scrabble Game.py

--- Number Scrabble Game.py
+++ Number Scrabble Game.py
@@ -1,10 +1,7 @@
-# File : CS112_A1_T2_2_20231143.py
 # Purpose : Number Scrabble is played with the list of numbers between 1 and 9. Each player takes turns picking a number
 #           from the list. Once a number has been picked, it cannot be picked again. If a player has picked three
 #           numbers that add up to 15, that player wins the game. However, if all the numbers are used and no player
 #           gets exactly 15, the game is a draw.
-# Author : Mohammed Atef Abd El-Kader Bayomi.
-# ID : 20231143
 # =============================================================================================== #
 
 from itertools import permutations
